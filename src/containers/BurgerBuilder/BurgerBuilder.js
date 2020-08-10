ab5c4fc4c615f3e89cccf16459173722e7ba8416
Panu99 <hegdepanu4@gmail.com>
2020-08-10 18:30:25 +0530

    ctt

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,10 +10,10 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
+    salad: 10.5,
+    cheese: 5.4,
     meat: 1.3,
-    bacon: 0.7
+    bacon: 4.7
 };
 
 class BurgerBuilder extends Component {
@@ -23,7 +23,7 @@ class BurgerBuilder extends Component {
     // }
     state = {
         ingredients: null,
-        totalPrice: 4,
+        totalPrice: 10,
         purchasable: false,
         purchasing: false,
         loading: false,
